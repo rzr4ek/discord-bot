c83ae1bd50b88da236a936f50766daa82d4e6424
rzr4ek <103784353+rzr4ek@users.noreply.github.com>
2025-02-25 06:52:38 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,6 @@ MTM0MzYwMTQ2NTEyMTMxMjkwOA.Gvkl48.A3CIn0bbzHm3WHrSUTJHqzlJDi5aUJWGN1G1Ls
 export OPENAI_API_KEY="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"
 python bot.py
 
-# Загружаем токены из .env файла
-load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Настройка OpenAI API
 openai.api_key = OPENAI_API_KEY
